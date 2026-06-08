3a391a2514390e02c8cc534cb883745d7387aaea
albert-dev <al96@hotmail.es>
2026-06-08 22:36:32 +0200

    refactor: "used the Interactor class to print messages"

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 const history = [];
 
 function lotteryResponseLogic() {
-    let randomNum = randomNumLogic();
-    let userInput = Number(Interactor.getHTML('user_input').value)
-    let responseElement = Interactor.getHTML("response")
-    let errorMessage = "Por favor introduce un número válido.";
-    let succesMessage = `Correcte, el teu número ${userInput} coincideix amb el de la loteria (${randomNum})!`
-    let tryAgainMessage = `El teu número ${userInput} no coincideix amb el de la loteria ${randomNum}, torna a intentar-ho!`
-    
-    if (userInput < 1 || userInput > 10 || isNaN(userInput)){
-        return responseElement.innerHTML= errorMessage
-    } else if (userInput === randomNum) {
-        history.push(succesMessage)
-        return responseElement.innerHTML = succesMessage
+  let randomNum = randomNumLogic();
+  let userInput = Number(Interactor.getHTML("user_input").value);
+  let responseElement = Interactor.getHTML("response");
+  let errorMessage = "Por favor introduce un número válido.";
+  let succesMessage = `Correcte, el teu número ${userInput} coincideix amb el de la loteria (${randomNum})!`;
+  let tryAgainMessage = `El teu número ${userInput} no coincideix amb el de la loteria ${randomNum}, torna a intentar-ho!`;
 
-        }
-      else {
-        history.push(tryAgainMessage)
-        return responseElement.innerHTML = tryAgainMessage
-
-    }
-
+  if (userInput < 1 || userInput > 10 || isNaN(userInput)) {
+    return Interactor.printMessage(errorMessage, responseElement);
+  } else if (userInput === randomNum) {
+    history.push(succesMessage);
+    return Interactor.printMessage(succesMessage, responseElement);
+  } else {
+    history.push(tryAgainMessage);
+    return Interactor.printMessage(tryAgainMessage, responseElement)
+  }
 }
-
