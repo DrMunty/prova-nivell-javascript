5ea8add2efc6cb581f8907957b0152286c831bf5
albert-dev <al96@hotmail.es>
2026-06-09 13:10:21 +0200

    refactor: "removed redundant input checking from lotteryResponseLogic, now its checkLotteryResults responsability"

--- src/scripts/Lottery-Response.js
+++ src/scripts/Lottery-Response.js
@@ -7,16 +7,17 @@ import { checkLotteryResult } from "./Lottery-Logic.js";
 export function lotteryResponseLogic() {
   const userInput = Number(Interactor.getHTML("user_input").value);
   const responseElement = Interactor.getHTML("response");
-
-  if (userInput < 1 || userInput > 10 || isNaN(userInput)) {
-    return Interactor.printMessage(Messages.errorMessage(), responseElement);
-  }
-
+  
   const randomNum = randomNumLogic();
 
-const message = checkLotteryResult(userInput, randomNum);
+  const message = checkLotteryResult(userInput, randomNum);
+
+  if (message !== Messages.errorMessage()){
 
     history.push(message);
+
+  }
+
 
     return Interactor.printMessage(message, responseElement);
 }
